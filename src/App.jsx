2f6d465e95be004c94e92f4747eb6a3d78c96b59
rashmichandrasekharan <rashmi2532002@gmail.com>
2024-07-09 16:37:52 +0530

    components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 
 import './App.css'
-
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Route,Routes } from 'react-router-dom'
+import Landing from './pages/Landing'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
 function App() {
   
 
   return (
     <>
-      
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/login' element={<Auth/>}/>
+        <Route path='/register' element={<Auth register/>}/>
+      </Routes>
+      <Footer/>
     </>
   )
 }
